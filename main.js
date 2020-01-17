c8402928edcb9e92f1678025af53459e076c7261
Peter Yu <peteryu@gmail.com>
2020-01-17 11:21:15 +0800

    playing around with the previous blocks now

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class Block{
     this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.data = data;
-    this.hash = this.calculateHash();
+    this.hash = this.calculateHash();Block
   }
   calculateHash() {
     return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
@@ -27,11 +27,32 @@ class Blockchain{
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+    if (currentBlock.hash !== currentBlock.calculateHash()) {
+      return false;
+    }
+    if (currentBlock.previousHash !== previousBlock.hash) {
+      return false;
+    }
+    if(this.chain[0] !== this.createGenesisBlock()) {
+      return false;
+    }
+    return true;
+    }
+  }
 }
 
-let savjeeCoin = newBlockchain();
+let savjeeCoin = new Blockchain();
 
 savjeeCoin.addBlock(new Block("20/07/2017", { amount: 4 }));
 savjeeCoin.addBlock(new Block("22/07/2017", { amount: 14 }));
+savjeeCoin.chain[1].data = { amount: 100 };
+savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash()
+
 
 console.log(JSON.stringify(savjeeCoin, null, 4));
+
+console.log('Blockchain valid? ' + savjeeCoin.isChainValid());
